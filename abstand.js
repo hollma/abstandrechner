c13c2553c9b32f43d35a5f105fe85db5dc6dce02
Mario Holldack <mario@holldack.org>
2018-12-22 22:55:55 +0100

    reference (S and R) vs. camera/car distance (A and K)

--- abstand.js
+++ abstand.js
@@ -30,7 +30,7 @@ var F = board.create('point', [108, 82], {
   size: 4,
   color: '#ffff00'
 }); // vanishing point
-var moveS = board.create('point', [18, 20], {
+var moveS = board.create('point', [142.5, 2], {
   name: 'moveS',
   size: 20,
   color: '#ffff00',
@@ -73,7 +73,7 @@ var rtrack = board.create('segment', ['F', 'moveR'], {
   fixed: true
 });
 // horizontal aux lines
-var srtrack = board.create('segment', [
+var srtrack = board.create('line', [
   [40, 35],
   [125, 35]
 ], {
@@ -81,7 +81,7 @@ var srtrack = board.create('segment', [
   strokeColor: '#ff00ff',
   strokeWidth: 6
 });
-var srtrackcopy = board.create('segment', [
+var srtrackcopy = board.create('line', [
   [70, 56],
   [117.5, 56]
 ], {
@@ -108,67 +108,59 @@ var atrack = board.create('segment', ['F', 'moveA'], {
 // aux points (copies)
 var Scopy = board.create('intersection', [ltrack, srtrackcopy, 0], {
   name: function() {
-    return '<span style="display: inline-block; font-size:12px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">S\' (' 
-+  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
+    return '<span style="display: inline-block; font-size:12px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">S\' (' +  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
   }
 });
 var Rcopoy = board.create('intersection', [rtrack, srtrackcopy, 0], {
   name: function() {
-    return '<span style="display: inline-block; font-size:12px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">R\' (' 
-+  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
+    return '<span style="display: inline-block; font-size:12px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">R\' (' +  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
   }
 });
 var Kcopy = board.create('intersection', [ktrack, srtrackcopy, 0], {
     name: function() {
-    return '<span style="display: inline-block; font-size:12px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">K\' (' 
-+  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
+    return '<span style="display: inline-block; font-size:12px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">K\' (' +  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
   }
 });
 var Acopy = board.create('intersection', [atrack, srtrackcopy, 0], {
   name: function() {
-    return '<span style="display: inline-block; font-size:12px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">A\' (' 
-+  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
+    return '<span style="display: inline-block; font-size:12px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">A\' (' +  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
   }
 });
 // copy of car position and camera projection points
 var S = board.create('intersection', [ltrack, srtrack, 0], {
   name: function() {
-    return '<span style="display: inline-block; font-size:20px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">S (' 
-+  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
+    return '<span style="display: inline-block; font-size:20px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">S (' +  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
   }
 });
 var A = board.create('intersection', [atrack, srtrack, 0], {
   name: function() {
-    return '<span style="display: inline-block; font-size:20px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">A (' 
-+  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
+    return '<span style="display: inline-block; font-size:20px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">A (' +  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
   }
 });
 var K = board.create('intersection', [ktrack, srtrack, 0], {
   name: function() {
-    return '<span style="display: inline-block; font-size:20px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">K (' 
-+  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
+    return '<span style="display: inline-block; font-size:20px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">K (' +  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
   }
 });
 var R = board.create('intersection', [rtrack, srtrack, 0], {
   name: function() {
-    return '<span style="display: inline-block; font-size:20px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">R (' 
-+  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
+    return '<span style="display: inline-block; font-size:20px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">R (' +  Math.round(this.X()*100)/100 + ', ' + Math.round(this.Y()*100)/100 + ')</span>';
   }
 });
 
 // calculations
 
-var results = board.create('point', [10, 80], {
+var results = board.create('point', [2, 90], {
   name: function() {
-    var SR_dist = Math.round( (R.X()-S.X()) * 100 )/100;
-    var SA_dist = Math.round( (A.X()-S.X()) * 100 )/100;
-    var SA_dist_ratio = SA_dist / SR_dist * 100;
-    var AK_dist = Math.round( (K.X()-A.X()) * 100 )/100;
-    var AK_dist_ratio = AK_dist / SR_dist * 100;
-    var KR_dist = Math.round( (R.X()-K.X()) * 100 )/100;
-    var KR_dist_ratio = KR_dist / SR_dist * 100;
+    var SR_dist = Math.abs(Math.round( (R.X()-S.X()) * 1000 )/1000);
+    // var SA_dist = Math.round( (A.X()-S.X()) * 100 )/100;
+    // var SA_dist_ratio = SA_dist / SR_dist * 100;
+    var AK_dist = Math.round( (K.X()-A.X()) * 1000 )/1000;
+    var AK_dist_ratio = AK_dist / SR_dist;
+    // var KR_dist = Math.round( (R.X()-K.X()) * 100 )/100;
+    // var KR_dist_ratio = KR_dist / SR_dist * 100;
 
-    return '<span style="display: inline-block; font-size:20px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">SR = ' +  SR_dist + '&nbsp;&nbsp;&nbsp;(100%)<br>SA = ' + SA_dist + '&nbsp;&nbsp;&nbsp;(' + Math.round(SA_dist_ratio*10)/10 + '%)<br>AK = ' + AK_dist + '&nbsp;&nbsp;&nbsp;(' + Math.round(AK_dist_ratio*10)/10 + '%)<br>KR = ' + KR_dist + '&nbsp;&nbsp;&nbsp;(' + Math.round(KR_dist_ratio*10)/10 + '%)</span>';
+    return '<span style="display: inline-block; font-size:12px; vertical-align: left; background-color:rgba(255,255,0,.5); left-margin: 0px;">SR = $TODO (get value from input field) ' +  SR_dist + '<br>AK = ' + Math.round(AK_dist_ratio*1000)/1000 + ' &times; SR</span>';
   },
   size: 0,
   color: '#ffffff',
